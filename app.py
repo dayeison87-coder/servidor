b7b38cc501c03d755a149d45544cff2619bb3198
dayeison87 <dayeison87@gmail.com>
2026-05-26 11:43:01 -0500

    Fix: Forzar formateo de placas a mayúsculas en producción

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# FASE 2: Ruta con el bug (guarda la placa tal como llega, incluso en minúsculas)
+# FASE 2: Ruta reparada (Fuerza de manera estricta que toda placa se guarde en MAYÚSCULAS)
 @app.route('/api/peritajes', methods=['POST'])
 def crear_peritaje():
     datos = request.get_json()
-    placa = datos.get('placa') # Aquí está el problema, no valida mayúsculas
+    placa = datos.get('placa') 
+    
+    # CIRUGÍA DE CÓDIGO: Aplicamos .upper() para transformar el string a mayúsculas estrictas
+    if placa:
+        placa = placa.upper() [cite: 33]
     
     # Simulación de guardar en la base de datos / JSON
     return jsonify({"mensaje": "Peritaje registrado", "placa_guardada": placa}), 201
